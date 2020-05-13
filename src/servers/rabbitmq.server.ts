6a0502f5682508b9cc429204444f1e99bd7003ac
Yuri Calabrez <yuri.calabrez@gmail.com>
2020-05-12 22:53:29 -0300

    Recebendo mensagens para os eventos do microsserviço de Vídeo

--- src/servers/rabbitmq.server.ts
+++ src/servers/rabbitmq.server.ts
@@ -1,6 +1,6 @@
 import {Server} from '@loopback/core';
 import {Context} from "@loopback/context"
-import {connect, Connection} from "amqplib"
+import {connect, Connection, Channel, Replies} from "amqplib"
 
 export class RabbitMqServer extends Context implements Server {
 
@@ -14,7 +14,27 @@ export class RabbitMqServer extends Context implements Server {
       password: 'admin'
     })
     this._listening = true
-    return undefined
+    this.boot()
+  }
+
+  async boot() {
+    const channel: Channel = await this.conn.createChannel()
+    const queue: Replies.AssertQueue = await channel.assertQueue('micro-catalog/sync-videos')
+    const exchange: Replies.AssertExchange = await channel.assertExchange('amq.topic', 'topic')
+
+    await channel.bindQueue(queue.queue, exchange.exchange, 'model.*.*')
+    //const result = channel.sendToQueue('my-first-queue', Buffer.from('hello world'))
+    //await channel.publish('amq.direct', 'minha-routing-key', Buffer.from('publicado por routing key'))
+
+    channel.consume(queue.queue, (message) => {
+      if (!message) {
+        return
+      }
+      console.log(JSON.parse(message.content.toString()))
+      const [model, event] = message.fields.routingKey.split('.').splice(1)
+      console.log(model, event)
+    })
+    //console.log(result)
   }
 
   async stop(): Promise<void> {
