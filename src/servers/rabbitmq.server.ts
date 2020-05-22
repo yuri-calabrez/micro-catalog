d87df8b2145c33cf8c246afb0770507c7b0d8178
Yuri Calabrez <yuri.calabrez@gmail.com>
2020-05-21 23:03:22 -0300

    Criando exchanges automaticamente

--- src/servers/rabbitmq.server.ts
+++ src/servers/rabbitmq.server.ts
@@ -1,31 +1,64 @@
 import {Server} from '@loopback/core';
-import {Context} from "@loopback/context"
-import {connect, Connection, Channel, Replies} from "amqplib"
+import {Context, inject} from "@loopback/context"
+import {Channel, Replies, Options, ConfirmChannel} from "amqplib"
 import {repository} from '@loopback/repository';
 import {CategoryRepository} from '../repositories';
 import {Category} from '../models';
+import {RabbitmqBindings} from '../keys';
+import {AmqpConnectionManagerOptions, AmqpConnectionManager, connect, ChannelWrapper} from 'amqp-connection-manager';
+
+export interface RabbitmqConfig {
+  uri: string
+  connOptions?: AmqpConnectionManagerOptions
+  exchanges?: {name: string, type: string, options?: Options.AssertExchange}[]
+}
 
 export class RabbitMqServer extends Context implements Server {
 
   private _listening: boolean;
-  conn: Connection
+  private _conn: AmqpConnectionManager
+  private _channelManager: ChannelWrapper
   channel: Channel
 
-  constructor(@repository(CategoryRepository) private categoryRepository: CategoryRepository) {
+  constructor(
+    @repository(CategoryRepository) private categoryRepository: CategoryRepository,
+    @inject(RabbitmqBindings.CONFIG) private config: RabbitmqConfig
+  ) {
     super()
   }
 
   async start(): Promise<void> {
-    this.conn = await connect({
-      hostname: 'rabbitmq',
-      username: 'admin',
-      password: 'admin'
+    this._conn = connect([this.config.uri], this.config.connOptions)
+    this._channelManager = this._conn.createChannel()
+
+    this.channelManager.on('connect', () => {
+      console.log("Connected to Rabbitmq!")
+      this._listening = true
     })
-    this._listening = true
-    this.boot()
+
+    this.channelManager.on('error', (err, {name}) => {
+      console.log(`Failed to setup a Rabbitmq channel - name:${name} | error:${err.message}`)
+      this._listening = false
+    })
+
+    await this.setupExchanges()
+    //this.boot()
+  }
+
+  private async setupExchanges() {
+    return this.channelManager.addSetup(async (channel: ConfirmChannel) => {
+      if (!this.config.exchanges) {
+        return
+      }
+
+      await Promise.all(this.config.exchanges.map((exchange) => (
+        channel.assertExchange(exchange.name, exchange.type, exchange.options)
+      )))
+    })
   }
 
   async boot() {
+    // @ts-ignore
     this.channel = await this.conn.createChannel()
     const queue: Replies.AssertQueue = await this.channel.assertQueue('micro-catalog/sync-videos')
     const exchange: Replies.AssertExchange = await this.channel.assertExchange('amq.topic', 'topic')
@@ -74,6 +107,14 @@ export class RabbitMqServer extends Context implements Server {
 
   get listening(): boolean {
     return this._listening
+  }
+
+  get conn(): AmqpConnectionManager {
+    return this._conn
+  }
+
+  get channelManager(): ChannelWrapper {
+    return this._channelManager
   }
 
 }
