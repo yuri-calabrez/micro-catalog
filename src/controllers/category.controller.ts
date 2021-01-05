d1aa1ca83e9524d62ea6be54113e38ce0d23dc13
Yuri Calabrez <yuri.calabrez@gmail.com>
2021-01-04 22:39:55 -0300

    Criando classe de paginaçao

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -7,16 +7,9 @@ import {
   Where,
 } from '@loopback/repository';
 import {
-  post,
   param,
   get,
-  getFilterSchemaFor,
   getModelSchemaRef,
-  getWhereSchemaFor,
-  patch,
-  put,
-  del,
-  requestBody,
 } from '@loopback/rest';
 import {Category} from '../models';
 import {CategoryRepository} from '../repositories';
@@ -24,32 +17,8 @@ import {CategoryRepository} from '../repositories';
 export class CategoryController {
   constructor(
     @repository(CategoryRepository)
-    public categoryRepository : CategoryRepository,
-  ) {}
-
-  @post('/categories', {
-    responses: {
-      '200': {
-        description: 'Category model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Category)}},
-      },
-    },
-  })
-  async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Category, {
-            title: 'NewCategory',
-            
-          }),
-        },
-      },
-    })
-    category: Category,
-  ): Promise<Category> {
-    return this.categoryRepository.create(category);
-  }
+    public categoryRepository: CategoryRepository,
+  ) { }
 
   @get('/categories/count', {
     responses: {
@@ -86,28 +55,6 @@ export class CategoryController {
     return this.categoryRepository.find(filter);
   }
 
-  @patch('/categories', {
-    responses: {
-      '200': {
-        description: 'Category PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Category, {partial: true}),
-        },
-      },
-    })
-    category: Category,
-    @param.where(Category) where?: Where<Category>,
-  ): Promise<Count> {
-    return this.categoryRepository.updateAll(category, where);
-  }
-
   @get('/categories/{id}', {
     responses: {
       '200': {
@@ -125,51 +72,5 @@ export class CategoryController {
     @param.filter(Category, {exclude: 'where'}) filter?: FilterExcludingWhere<Category>
   ): Promise<Category> {
     return this.categoryRepository.findById(id, filter);
-  }
-
-  @patch('/categories/{id}', {
-    responses: {
-      '204': {
-        description: 'Category PATCH success',
-      },
-    },
-  })
-  async updateById(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Category, {partial: true}),
-        },
-      },
-    })
-    category: Category,
-  ): Promise<void> {
-    await this.categoryRepository.updateById(id, category);
-  }
-
-  @put('/categories/{id}', {
-    responses: {
-      '204': {
-        description: 'Category PUT success',
-      },
-    },
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() category: Category,
-  ): Promise<void> {
-    await this.categoryRepository.replaceById(id, category);
-  }
-
-  @del('/categories/{id}', {
-    responses: {
-      '204': {
-        description: 'Category DELETE success',
-      },
-    },
-  })
-  async deleteById(@param.path.string('id') id: string): Promise<void> {
-    await this.categoryRepository.deleteById(id);
   }
 }
